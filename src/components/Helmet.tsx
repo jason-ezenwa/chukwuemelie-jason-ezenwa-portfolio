ae3544d0205a0ecaa891f6e0383f764fcb2e2082
JasonFlair <emelieobumse100@gmail.com>
2024-07-08 01:36:27 +0100

    refactor: Update favicon and URLs in Helmet component

--- src/components/Helmet.tsx
+++ src/components/Helmet.tsx
@@ -6,22 +6,22 @@ export default function Helmet() {
       <title>Chukwuemelie Obumse | Software Engineer</title>
       <meta name="keywords" content="jason-ezenwa, Chukwuemelie Obumse, Full Stack developer, React developer, Node.js developer, Python developer, backend engineer" />
       <meta name="description" content="Chukwuemelie Obumse | Full Stack Software Engineer with two years of experience in software development, specialized in backend development and frontend development, experienced in DevOps and database management. Adept at crafting clean and testable code, to develop and seamlessly integrate secure RESTful and GraphQL API paradigms, with frontend mobile and web applications. Skilled in integrating data analytics tools to support informed business decisions." />
-      <link rel="icon" type="image/png" href="/assets/DevFolio.PNG" />
+      <link rel="icon" type="image/ico" href="/favicon.ico" />
       <meta name="theme-color" content="#36d1dc" />
 
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://flair-portfolio.vercel.app/" />
+      <meta property="og:url" content="https://jason-ezenwa.vercel.app/" />
       <meta property="og:title" content="Chukwuemelie Obumse | Full Stack Software Engineer" />
       <meta property="og:description" content="Portfolio website for Chukwuemelie Obumse | Full Stack Software Engineer" />
-      <meta property="og:image" content="https://flair-portfolio.vercel.app/assets/DevFolio.PNG" />
+      <meta property="og:image" content="https://jason-ezenwa.vercel.app/favicon.ico" />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://flair-portfolio.vercel.app/" />
+      <meta property="twitter:url" content="https://jason-ezenwa.vercel.app/" />
       <meta property="twitter:title" content="Chukwuemelie Obumse | Full Stack Software Engineer" />
       <meta property="twitter:description" content="Portfolio website for Chukwuemelie Obumse | Full Stack Software Engineer" />
-      <meta property="twitter:image" content="https://flair-portfolio.vercel.app/assets/DevFolio.PNG" />
+      <meta property="twitter:image" content="https://jason-ezenwa.vercel.app/favicon.ico" />
     </Head>
   );
 }
