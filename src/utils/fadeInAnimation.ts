1a5904892414c18903ff7bfba9b24323e27df0e3
JasonFlair <emelieobumse100@gmail.com>
2024-07-08 08:04:32 +0100

    update: Adjust fade in animation direction in fadeInAnimation.ts

--- src/utils/fadeInAnimation.ts
+++ src/utils/fadeInAnimation.ts
@@ -2,7 +2,7 @@ export const fadeIn = (direction: string, delay: any) => {
   return {
     hidden: {
       y: direction === "up" ? 120 : direction === "down" ? -120 : 0,
-      x: direction === "left" ? 120 : direction === "right" ? -120 : 0,
+      x: direction === "left" ? 180 : direction === "right" ? -180 : 0,
     },
     show: {
       y: 0,
