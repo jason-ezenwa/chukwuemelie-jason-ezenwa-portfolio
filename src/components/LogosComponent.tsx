299663fb0b30540aeeb653b5ef33273957719798
JasonFlair <emelieobumse100@gmail.com>
2024-07-12 20:31:32 +0100

    refactor: Update logo component dimensions for improved responsiveness and clarity

--- src/components/LogosComponent.tsx
+++ src/components/LogosComponent.tsx
@@ -15,7 +15,7 @@ const LogosComponent: React.FC = () => {
           {logos.map((logo, index) => (
             <div
             key={index}
-              className="relative w-[1000px] lg:w-[1350px]  h-[85px] sm:h-[85px] lg:h-[110px]"
+              className="relative w-[850px] lg:w-[1350px] h-[80px] sm:h-[80px] lg:h-[110px]"
             >
               <Image
                 src={logo}
