6e061a892412ce91005d0c24d08f7a16457aa11d
JasonFlair <emelieobumse100@gmail.com>
2024-07-13 09:15:43 +0100

    refactor: Update logo component dimensions for improved responsiveness and clarity

--- src/components/LogosComponent.tsx
+++ src/components/LogosComponent.tsx
@@ -20,7 +20,7 @@ const LogosComponent: React.FC = () => {
   return (
     <div className="w-[98vw]">
       <Marquee>
-        <div className="flex space-x-8 md:space-x-16 pr-4">
+        <div className="flex space-x-6 md:space-x-16 pr-5">
           {logos.map((logo, index) => (
             <div
             key={index}
