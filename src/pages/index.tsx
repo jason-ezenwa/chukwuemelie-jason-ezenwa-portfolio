a00e3b445178fbff8984c9ecaf016b0ba879d5d2
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2024-09-21 14:37:50 +0100

    feat: update link to "Beyond Silos" blog post.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -176,7 +176,7 @@ export default function Home() {
 									"In this piece, I talk about how engineers work across disciplines to build smarter, more impactful solutions over time.",
 								]}
 								image="/images/software_engineer_doing_a_presentation.jpeg"
-								link="https://blog.stackademic.com/the-priceless-influence-of-senior-software-engineers-on-junior-engineers-c7c7cac89747"
+								link="https://medium.com/@emelieobumse/beyond-silos-the-cross-disciplinary-role-of-the-modern-software-engineer-23305b55389b"
 							/>
 							<BlogCard
 								title="The Priceless Influence of Senior Software Engineers on Junior Engineers"
