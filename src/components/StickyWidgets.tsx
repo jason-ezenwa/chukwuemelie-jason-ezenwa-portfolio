521fac90e0434e1f5b17f0010ffc6113ba527d67
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2024-11-04 11:13:54 +0100

    chore: animate bottom right widgets

--- src/components/StickyWidgets.tsx
+++ src/components/StickyWidgets.tsx
@@ -24,10 +24,10 @@ interface StickyBottomRightProps {
 
 const StickyBottomRight: React.FC<StickyBottomRightProps> = ({ children }) => {
   return (
-    <div className="z-20 fixed bottom-0 md:bottom-16 right-0 md:right-1 m-2 space-y-2 md:space-y-2 flex flex-col items-end">
-      {children}
-    </div>
-  );
+		<div className="z-20 fixed bottom-0 md:bottom-16 right-0 md:right-1 m-2 space-y-2 md:space-y-2 flex flex-col items-end animate-bounce hover:animate-none">
+			{children}
+		</div>
+	);
 };
 
 export { StickyBottomRight, StickyDiv };
