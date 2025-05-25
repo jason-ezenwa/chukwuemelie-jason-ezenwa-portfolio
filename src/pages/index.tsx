8bf7aceed454b7b140cbc708330d4e68f1948785
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2025-05-25 01:14:32 +0100

    refactor: refactor presentation of zen finance.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,9 +45,9 @@ export default function Home() {
         {/* Hero section */}
         <section className="pt-16 px-6 flex flex-col items-center transition-colors duration-300">
           <motion.div
-            variants={fadeIn('up', 0.1)}
+            variants={fadeIn("up", 0.1)}
             initial="hidden"
-            whileInView={'show'}
+            whileInView={"show"}
             viewport={{ once: true, amount: 0.3 }}
             className={`${dmSans.className} py-20 flex flex-col gap-4 lg:gap-8`}>
             <h1 className="font-extrabold text-4xl md:text-5xl lg:text-6xl">
@@ -69,9 +69,9 @@ export default function Home() {
               </p>
             </div>
             <motion.div
-              variants={fadeIn('left', 0.3)}
+              variants={fadeIn("left", 0.3)}
               initial="hidden"
-              whileInView={'show'}
+              whileInView={"show"}
               viewport={{ once: true, amount: 0.005 }}
               className="w-full 2xl:w-[90%]  bg-white border-dotted border-l-[1px] border-black px-4 py-4 text-[#344054] shadow-sm hover:shadow-md">
               <p className="text-base lg:text-lg text-[#344054]">{`I am a Full-Stack Software Engineer with 3+ years of experience delivering scalable and maintainable solutions across B2B and B2C domains. I specialize in backend and frontend engineering, with additional strengths in system design, DevOps, and database management.`}</p>
@@ -108,9 +108,9 @@ export default function Home() {
               </p>
             </div>
             <motion.div
-              variants={fadeIn('up', 0.05)}
+              variants={fadeIn("up", 0.05)}
               initial="hidden"
-              whileInView={'show'}
+              whileInView={"show"}
               viewport={{ once: true, amount: 0.005 }}
               className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8">
               <ImpactStoryCard
@@ -119,9 +119,9 @@ export default function Home() {
                 techStack="TypeScript, React, Node.js, GraphQL, MongoDB, AWS"
                 duration="Jan 2024 - Present"
                 keyContributions={[
-                  'Built systems for artisan recruitment, home maintenance, and AI-driven portfolios',
-                  'Provided data-driven insights that guided product improvements and optimized funnels.',
-                  'Improved platform SEO and organic traffic by 28%',
+                  "Built systems for artisan recruitment, home maintenance, and AI-driven portfolios",
+                  "Provided data-driven insights that guided product improvements and optimized funnels.",
+                  "Improved platform SEO and organic traffic by 28%",
                 ]}
                 link="/impact-stories#blue-collar-empowerment"
               />
@@ -131,9 +131,9 @@ export default function Home() {
                 techStack="Next.js, TypeScript"
                 duration="Apr 2024 - Apr 2025"
                 keyContributions={[
-                  'Built an application that onboarded 10,000+ testers in a month',
-                  'Developed product feedback application for better data-driven decisions',
-                  'Led technical assessment of new engineering hires',
+                  "Built an application that onboarded 10,000+ testers in a month",
+                  "Developed product feedback application for better data-driven decisions",
+                  "Led technical assessment of new engineering hires",
                 ]}
                 link="/impact-stories#product-feedback"
               />
@@ -143,9 +143,9 @@ export default function Home() {
                 techStack="Next.js, Node.js, TypeScript, PostgreSQL, Docker"
                 duration="Aug 2024 - Present"
                 keyContributions={[
-                  'Leading development of a gold sourcing and purchase platform',
-                  'Building secure wallet and authentication systems',
-                  'Shaping secure, scalable infrastructure for gold investment platform',
+                  "Leading development of a gold sourcing and purchase platform",
+                  "Building secure wallet and authentication systems",
+                  "Shaping secure, scalable infrastructure for gold investment platform",
                 ]}
                 link="/impact-stories#gold-investment"
               />
@@ -169,32 +169,33 @@ export default function Home() {
               </p>
             </div>
             <motion.div
-              variants={fadeIn('right', 0.05)}
+              variants={fadeIn("right", 0.05)}
               initial="hidden"
-              whileInView={'show'}
+              whileInView={"show"}
               viewport={{ once: true, amount: 0.005 }}
               className="bg-center lg:bg-right-bottom bg-no-repeat mt-4 grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-8">
+              <ProjectCard
+                title="Zen"
+                descriptionParagraphs={[
+                  "Zen Finance is a modern fintech application that allows users to manage multi-currency wallets, exchange currencies, and access virtual card services such as creation, funding, and transaction tracking. It integrates with Maplerad and Paystack.",
+                  "Built with: TypeScript, Next.js, Node.js, MongoDB",
+                ]}
+                image="/images/pay-online-illustration.jpeg"
+                githubLink="https://github.com/jason-ezenwa/zen-finance"
+                seeLiveLink="https://try-zen-finance.vercel.app/"
+              />
               <ProjectCard
                 title="WYNK.NG"
                 descriptionParagraphs={[
                   "I rebuilt Wynk's web application while working as an intern at Wynk Limited.",
-                  'This web application displays and elaborates on the services provided by Wynk, from ride hailing to payment and lifestyle services, as well as the opportunities in store for partners and potential partners.',
-                  'Built with: React, Tailwind CSS, Material UI',
+                  "This web application displays and elaborates on the services provided by Wynk, from ride hailing to payment and lifestyle services, as well as the opportunities in store for partners and potential partners.",
+                  "Built with: React, Tailwind CSS, Material UI",
                 ]}
                 image="/images/wynk-landing-page.PNG"
                 githubLink=""
                 seeLiveLink="https://wynk.ng/"
               />
-              <ProjectCard
-                title="Zen"
-                descriptionParagraphs={[
-                  "This is an API designed for fintech applications. It provides features that allow users to have multicurrency wallets, exchange currencies and generate virtual cards. Zen integrates Maplerad's sandbox environment to power most of its features.",
-                  'Built with: TypeScript, Node JS, Express JS, MongoDB',
-                ]}
-                image="/images/pay-online-illustration.jpeg"
-                githubLink="https://github.com/jason-ezenwa/zen-api"
-                seeLiveLink="https://zen-api-68zd.onrender.com"
-              />
+
               {/* <ProjectCard
                 title="Vibes Tailor"
                 descriptionParagraphs={[
@@ -219,24 +220,24 @@ export default function Home() {
               </p>
             </div>
             <motion.div
-              variants={fadeIn('left', 0.05)}
+              variants={fadeIn("left", 0.05)}
               initial="hidden"
-              whileInView={'show'}
+              whileInView={"show"}
               viewport={{ once: true, amount: 0.005 }}
               className="bg-center lg:bg-right-bottom bg-no-repeat mt-4 grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8">
               <BlogCard
                 title="Beyond Silos: The Cross-Disciplinary Role of the Modern Software Engineer"
                 descriptionParagraphs={[
-                  'An article about the exposure of software engineers to different fields while working with experts in those fields.',
-                  'In this piece, I talk about how engineers work across disciplines to build smarter, more impactful solutions over time.',
+                  "An article about the exposure of software engineers to different fields while working with experts in those fields.",
+                  "In this piece, I talk about how engineers work across disciplines to build smarter, more impactful solutions over time.",
                 ]}
                 link="https://blog.stackademic.com/beyond-silos-the-cross-disciplinary-role-of-the-modern-software-engineer-23305b55389b"
               />
               <BlogCard
                 title="The Priceless Influence of Senior Software Engineers on Junior Engineers"
                 descriptionParagraphs={[
-                  'An article that highlights the multifaceted benefits of working with senior software engineers.',
-                  'In this piece, I explore how the mentorship and guidance from experienced engineers can dramatically accelerate the growth and expertise of junior engineers.',
+                  "An article that highlights the multifaceted benefits of working with senior software engineers.",
+                  "In this piece, I explore how the mentorship and guidance from experienced engineers can dramatically accelerate the growth and expertise of junior engineers.",
                 ]}
                 link="https://blog.stackademic.com/the-priceless-influence-of-senior-software-engineers-on-junior-engineers-c7c7cac89747"
               />
@@ -244,7 +245,7 @@ export default function Home() {
                 title="Authentication and Authorization with JSON Web Tokens"
                 descriptionParagraphs={[
                   "This blog dives deep into the world of JSON Web Tokens (JWTs), shedding light on how they're used in authentication and authorization processes.",
-                  'In this blog post, I explain: What JWTs are and how they work, from token issuance to server verification.',
+                  "In this blog post, I explain: What JWTs are and how they work, from token issuance to server verification.",
                 ]}
                 link="https://medium.com/stackademic/the-definitive-guide-to-authentication-and-authorization-with-json-web-tokens-everything-you-need-5bc7795cb668"
               />
@@ -252,23 +253,23 @@ export default function Home() {
                 title="Getting Familiar with Props in React"
                 descriptionParagraphs={[
                   "This is a guide that explains what 'props' are, and how they work. Taking you on a journey to transform your components from static to dynamic.",
-                  'In this blog post, I explain: How to pass data between components using props as well as integration with databases.',
+                  "In this blog post, I explain: How to pass data between components using props as well as integration with databases.",
                 ]}
                 link="https://medium.com/@emelieobumse100/getting-familiar-with-props-in-react-5abdcaae8b04"
               />
               <BlogCard
                 title="Implementing an Escrow-like State in Node.js: Building Trust and Security in Online Transactions"
                 descriptionParagraphs={[
-                  'Explains the concept of escrows and a nodejs implementation',
-                  'In this blog post, I go through: What escrows are and their importance as well as a step by step implementation in Node.js.',
+                  "Explains the concept of escrows and a nodejs implementation",
+                  "In this blog post, I go through: What escrows are and their importance as well as a step by step implementation in Node.js.",
                 ]}
                 link="https://medium.com/@emelieobumse100/building-flair-vibes-tailor-a-personalised-music-recommender-c3a0f042faa2"
               />
               <BlogCard
                 title='What Happens When You Type "https://www.google.com" Into Your Browser and Press Enter'
                 descriptionParagraphs={[
-                  'This blog post, walks through the various steps involved in loading the webpage you see when you try to use google.',
-                  'In this blog post, I explain: The DNS process, what it is and how it occurs as well as web servers, application servers and load balancers.',
+                  "This blog post, walks through the various steps involved in loading the webpage you see when you try to use google.",
+                  "In this blog post, I explain: The DNS process, what it is and how it occurs as well as web servers, application servers and load balancers.",
                 ]}
                 link="https://medium.com/@emelieobumse100/what-happens-when-you-type-https-www-google-com-into-your-browser-and-press-enter-bc0b0d8c7d08"
               />
